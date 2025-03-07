1252e2d7f5c6a7a64340364da03f9ed5e65678b0
adamh <hoffmann.adam@blathy.info>
2025-03-07 13:14:21 +0100

    menu

--- DrawerOOP2.0/Menu.cs
+++ DrawerOOP2.0/Menu.cs
@@ -17,10 +17,10 @@ namespace DrawerOOP2._0
         {
             options = new List<MenuOption>
             {
-                new MenuOption("Új", () => new DrawingManager().StartDrawing()),
+                new MenuOption("      Új      ", () => new DrawingManager().StartDrawing()),
                 new MenuOption("Mentett rajzok", () => new SaveManager().LoadDrawing()),
-                new MenuOption("Törlés", () => new DeleteManager().DeleteDrawing()),
-                new MenuOption("Kilépés", () => Environment.Exit(0))
+                new MenuOption("    Törlés    ", () => new DeleteManager().DeleteDrawing()),
+                new MenuOption("    Kilépés   ", () => Environment.Exit(0))
             };
 
             renderer = new MenuRenderer(options);
