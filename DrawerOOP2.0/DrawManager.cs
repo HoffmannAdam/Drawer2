170d95bd793806aedae63ef041cbc08f905898e4
adamh <hoffmann.adam@blathy.info>
2025-03-13 21:28:26 +0100

    idkkkkkkkk

--- DrawerOOP2.0/DrawManager.cs
+++ DrawerOOP2.0/DrawManager.cs
@@ -9,10 +9,92 @@ namespace DrawerOOP2._0
     internal class DrawingManager
     {
         private static Menu menu = new Menu();
-        
+        private static SaveManager saveManager = new SaveManager();
+        private List<Drawing> drawings = new List<Drawing>();
+
         public void StartDrawing()
         {
-            
+            Console.Clear();
+            Console.CursorVisible = true;
+            Console.Write("1-Fehér 2-Zöld 3-Sárga 4-Piros 5-Kék 6-Cián 7-Magenta Space-Rajzolás Del-Törlés");
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+
+            int x = 0, y = 0;
+            ConsoleColor currentColor = ConsoleColor.White;
+            ConsoleKey key;
+
+            do
+            {
+                Console.SetCursorPosition(x, y);
+                key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (y > 0) y--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (y < Console.WindowHeight - 2) y++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (x > 0) x--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (x < Console.WindowWidth - 1) x++;
+                        break;
+                    case ConsoleKey.Spacebar:
+                        Console.ForegroundColor = currentColor;
+                        Console.Write('█');
+                        drawings.Add(new Drawing(x, y, currentColor));
+                        break;
+                    case ConsoleKey.Delete:
+                        Console.Write(" ");
+                        drawings.RemoveAll(d => d.X == x && d.Y == y);
+                        break;
+                }
+
+                switch (key)
+                {
+                    case ConsoleKey.D1: currentColor = ConsoleColor.White; break;
+                    case ConsoleKey.D2: currentColor = ConsoleColor.Green; break;
+                    case ConsoleKey.D3: currentColor = ConsoleColor.Yellow; break;
+                    case ConsoleKey.D4: currentColor = ConsoleColor.Red; break;
+                    case ConsoleKey.D5: currentColor = ConsoleColor.Blue; break;
+                    case ConsoleKey.D6: currentColor = ConsoleColor.Cyan; break;
+                    case ConsoleKey.D7: currentColor = ConsoleColor.Magenta; break;
+                }
+
+                if (Console.CapsLock)
+                {
+                    Console.ForegroundColor = currentColor;
+                    Console.Write('█');
+                    drawings.Add(new Drawing(x, y, currentColor));
+                }
+
+            } while (key != ConsoleKey.Escape);
+
+            saveManager.SaveDrawing(drawings);
+
+            if (key == ConsoleKey.Escape)
+            {
+                Console.ResetColor();
+                Console.Clear();
+                menu.Display();
+            }
+        }
+    }
+
+    internal class Drawing
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Drawing(int x, int y, ConsoleColor color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
         }
     }
 }
--- DrawerOOP2.0/SaveManager.cs
+++ DrawerOOP2.0/SaveManager.cs
@@ -10,15 +10,71 @@ namespace DrawerOOP2._0
     {
         private static Menu menu = new Menu();
 
-        public void SaveDrawing()
+        public void SaveDrawing(List<Drawing> drawings)
         {
-            
+            string fileName = "rajz1.txt";
+            int index = 1;
+
+            while (File.Exists(fileName))
+                fileName = $"rajz{++index}.txt";
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                foreach (var d in drawings)
+                    writer.WriteLine($"{d.X},{d.Y},{d.Color}");
+            }
+
+            Console.SetCursorPosition(0, Console.WindowHeight - 2);
+            Console.WriteLine($"A rajz '{fileName}' néven elmentve.");
+            Console.ReadKey();
         }
 
         public void LoadDrawing()
         {
+            Console.Clear();
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), "rajz*.txt");
+            if (files.Length == 0)
+            {
+                Console.WriteLine("Nincsenek mentett rajzok.");
+                Console.ReadKey();
+                return;
+            }
 
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+            Console.WriteLine("Válassz egy rajzot (Enter-rel megnyitod, Esc-kilép)");
+            int selectedIndex = 0;
+            ConsoleKey key;
+
+            do
+            {
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    Console.SetCursorPosition(0, i);
+                    if (i == selectedIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("> " + Path.GetFileName(files[i]));
+                    }
+                    else
+                    {
+                        Console.Write("  " + Path.GetFileName(files[i]));
+                    }
+                    Console.ResetColor();
+                }
+
+                key = Console.ReadKey(true).Key;
+                if (key == ConsoleKey.UpArrow && selectedIndex > 0) selectedIndex--;
+                if (key == ConsoleKey.DownArrow && selectedIndex < files.Length - 1) selectedIndex++;
+                if (key == ConsoleKey.Enter) return;
+            } while (key != ConsoleKey.Escape);
+
+            if (key == ConsoleKey.Escape)
+            {
+                Console.ResetColor();
+                Console.Clear();
+                menu.Display();
+            }
         }
     }
-
 }
